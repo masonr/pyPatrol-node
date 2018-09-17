3c6ca42d8e83a9d0368aa2a0b8e7108c6d7aaa6b
Mason Rowe <mason@rowe.sh>
2018-09-16 23:22:21 -0400

    Updates to match pyPatrol-server requirements

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
 from sanic import Sanic
 from sanic.response import json
-import threading, requests, time, json
+import threading, time, json, socket, sys
 import routes
 
 app = Sanic(strict_slashes=True)
 routes.routes(app)
 
 # config vars
+node_name="PYPATROL_NODE1"		# descriptor of the pyPatrol node
 node_ip="127.0.0.1"				# ip or hostname of the pyPatrol node
 node_port=12345					# port of the pyPatrol node
-mothership_host="127.0.0.1"		# ip or hostname of the pyMothership server
-mothership_port=12346			# port of the pyMothership server
+server_host="127.0.0.1"			# ip or hostname of the pyPatrol server
+server_port=12355				# port of the pyPatrol server
+server_secret="secret"			# secret keyword for auth to pyPatrol server
 ipv4_capable=True				# pyPatrol node ipv4 capable?
 ipv6_capable=True				# pyPatrol node ipv6 capable?
-standalone=True					# standalone mode or connect to pyMothership server
+standalone=False				# standalone mode or connect to pyPatrol server
 num_of_workers=10				# number of async workers
 use_ssl=False					# set if SSL (HTTPS) connections should be utilized
-if use_ssl:					# fill in SSL certificate information if use_ssl is True
+if use_ssl:						# fill in SSL certificate information if use_ssl is True
 	ssl = {'cert': "/path/to/cert", 'key': "/path/to/keyfile"}
 
 def heartbeat():
-	base = 'https://' if use_ssl else 'http://'
-	uri = base + mothership_host + ':' + str(mothership_port)
-	data = {'ipv4': ipv4_capable,
+	data = {
+			'name': node_name,
+			'ip': node_ip,
+			'port': node_port,
+			'ipv4': ipv4_capable,
 			'ipv6': ipv6_capable,
-			'ssl': use_ssl
+			'ssl': use_ssl,
+			'secret': server_secret
 			}
-	headers = {'content-type': 'application/json'}
 
 	while True:
-		requests.post(uri, data=json.dumps(data), headers=headers)
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock.connect((server_host, server_port))
+			sock.send(json.dumps(data).encode())
+		except:
+			print('Unable to establish connection to the server. Sleeping...')
+		finally:
+			sock.close()
 		time.sleep(30) # sleep for 30s
 
 if not standalone:
